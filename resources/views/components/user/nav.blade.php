d1b2fffbc71462223fd3acb7b96f71944725f082
portableTiavina <setanirinatiavina@gmail.com>
2026-02-16 11:12:34 +0400

    maj nav user

--- resources/views/components/user/nav.blade.php
+++ resources/views/components/user/nav.blade.php
@@ -11,9 +11,6 @@
     <a href="{{ route('user.courses.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-xl {{ request()->routeIs('user.courses.index') ? 'bg-primary/10 text-primary font-bold' : 'text-text-sub-light hover:bg-white hover:text-primary' }} transition-all">
         <span class="material-symbols-outlined">school</span> Parcourir les cours
     </a>
-    <a href="{{ route('user.bookings.one-to-one.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-xl {{ request()->routeIs('user.bookings.one-to-one.index') ? 'bg-primary/10 text-primary font-bold' : 'text-text-sub-light hover:bg-white hover:text-primary' }} transition-all">
-        <span class="material-symbols-outlined">schedule</span> Réserver un RDV
-    </a>
     <a href="{{ route('user.bookings.index') }}" class="flex items-center gap-3 px-4 py-3 rounded-xl {{ request()->routeIs('user.bookings.index') ? 'bg-primary/10 text-primary font-bold' : 'text-text-sub-light hover:bg-white hover:text-primary' }} transition-all">
         <span class="material-symbols-outlined">calendar_today</span> Mes Réservations
     </a>
